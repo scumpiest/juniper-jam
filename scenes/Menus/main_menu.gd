d820d0042e9c289b1e7add32d9fe66ee30c96c1a
Dayumm_555 <joshuaderek93@gmail.com>
2026-06-26 19:47:22 +0100

    added audio effects

--- scenes/Menus/main_menu.gd
+++ scenes/Menus/main_menu.gd
@@ -1,16 +1,26 @@
 extends CanvasLayer
+@onready var button_hover: AudioStreamPlayer = $ButtonHover
+@onready var button_clicked: AudioStreamPlayer = $ButtonClicked
 
 
 func _on_play_button_pressed() -> void:
+	button_clicked.play()
+	await button_clicked.finished
 	LevelTransition.change_scene_to("res://scenes/Cutscenes/cutscene.tscn")
 
 
 
 func _on_options_button_pressed() -> void:
+	button_clicked.play()
+	await button_clicked.finished
 	LevelTransition.change_scene_to("res://scenes/Menus/settings_menu.tscn")
-
 
 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_mouse_entered() -> void:
+	button_hover.play()
