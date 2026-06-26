146d5d920265823b1fb1975d2eb9301bb94d8012
Emir <saberfang961@gmail.com>
2026-06-26 20:48:28 +0200

    fix: resource ordering

--- globals/product_database.gd
+++ globals/product_database.gd
@@ -1,14 +1,18 @@
 extends Node
 
-@export var products: Array[ProductResource] = [
-	preload("res://resources/products/A.tres"),
-	preload("res://resources/products/B.tres"),
-	preload("res://resources/products/C.tres"),
-	preload("res://resources/products/AB.tres"),
-	preload("res://resources/products/BC.tres"),
-	preload("res://resources/products/CA.tres"),
+const DISPLAY_PLANTS: Array[PlantResource] = [
+	PlantDatabase.A,
+	PlantDatabase.B,
+	PlantDatabase.C,
+	PlantDatabase.AB,
+	PlantDatabase.BC,
+	PlantDatabase.CA,
 ]
 
 
 func get_all_products() -> Array[ProductResource]:
+	var products: Array[ProductResource] = []
+	for plant: PlantResource in DISPLAY_PLANTS:
+		if plant.product != null:
+			products.append(plant.product)
 	return products
